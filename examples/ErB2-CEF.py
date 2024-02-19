dc0e3118c7373e376391c88eff8b7724b2aa8f54
mstekiel <64463131+mstekiel@users.noreply.github.com>
2024-02-19 11:31:22 +0100

    Update examples

--- examples/ErB2-CEF.py
+++ examples/ErB2-CEF.py
@@ -6,21 +6,19 @@ from scipy.optimize import curve_fit
 from timeit import default_timer as timer
 
 import sys
-sys.path.append('C:/Users/Stekiel/Documents/GitHub/mikibox')
-import mikibox as ms
+sys.path.append('C:/Users/Stekiel/Documents/GitHub/crysfipy')
+import crysfipy
 
 
 # Analysis of the crystal field effects in CePdAl3
-lattice = ms.crystallography.Lattice([3.275,3.275,3.784,90,90,120])
-
 calculateMagnetism = True
 
 Bij = [1.26987, -0.00062433, -2.86435e-07, 1.01125e-05]
 # Bij = [1.26987, 0, 0, 0]
 
-CEFpars = ms.crysfipy.CEFpars('D6h', Bij, 'meV')
+CEFpars = crysfipy.CEFpars('D6h', Bij, 'meV')
 
-cefion_ErB2 = ms.crysfipy.CEFion(ms.crysfipy.Ion('Er'),[0,0,0], CEFpars)
+cefion_ErB2 = crysfipy.CEFion(crysfipy.Ion('Er'),[0,0,0], CEFpars)
 cefion = cefion_ErB2
 print(CEFpars)
 print(cefion_ErB2.cfp)
@@ -36,18 +34,36 @@ print(cefion_ErB2)
 if calculateMagnetism:
     temperature=0.1
     temperatures = np.linspace(1,300,601)
+    kB = 8.61733e-2 # in meV/K
 
-    fig, axs = plt.subplots(figsize=(5,10), nrows=3, tight_layout=True)
 
+    fig, axs = plt.subplots(figsize=(5,13), nrows=4, tight_layout=True)
 
-    # Set up the susceptibility calculated from magnetization
+    # Specific heat
     ax = axs[0]
 
     ax.set_xlabel('Temperature (K)')
     ax.set_ylabel('Magnetic susceptibility ($\mu_B^2$/ion)')
 
-    chi_100 = ms.crysfipy.susceptibility(cefion, temperatures , (1,0,0), method='magnetization')
-    chi_001 = ms.crysfipy.susceptibility(cefion, temperatures , (0,0,1), method='magnetization')
+    _, _, _, _, cp = crysfipy.thermodynamics(cefion, temperatures)
+    for ecef in cefion.energies:
+        Tcef =ecef/kB
+        if Tcef < temperatures.max():
+            ax.axvline(Tcef, 0, 0.5, color='red')
+
+    ax.plot(temperatures, cp, marker='d', color='black', label='cp')
+
+    ax.legend(title='Specific heat')
+
+
+    # Set up the susceptibility calculated from magnetization
+    ax = axs[1]
+
+    ax.set_xlabel('Temperature (K)')
+    ax.set_ylabel('Magnetic susceptibility ($\mu_B^2$/ion)')
+
+    chi_100 = crysfipy.susceptibility(cefion, temperatures , (1,0,0), method='magnetization')
+    chi_001 = crysfipy.susceptibility(cefion, temperatures , (0,0,1), method='magnetization')
 
     ax.plot(temperatures, chi_100, marker='^', color='red', label='$\chi$ || [100]', mfc=None)
     ax.plot(temperatures, chi_001, marker='o', color='blue', label='$\chi$ || [001]', mfc=None)
@@ -56,15 +72,15 @@ if calculateMagnetism:
     ax.legend(title=f'ErB$_2$ magnetization')
 
     # Set up the susceptibility calculated from perturbation
-    ax = axs[1]
+    ax = axs[2]
 
     ax.set_xlabel('Temperature (K)')
     ax.set_ylabel('Magnetic susceptibility ($\mu_B^2$/ion)')
     #ax.set_ylim(0,4)
 
-    chi_100 = ms.crysfipy.susceptibility(cefion, temperatures , (1,0,0), method='perturbation')
-    chi_010 = ms.crysfipy.susceptibility(cefion, temperatures , (0,1,0), method='perturbation')
-    chi_001 = ms.crysfipy.susceptibility(cefion, temperatures , (0,0,1), method='perturbation')
+    chi_100 = crysfipy.susceptibility(cefion, temperatures , (1,0,0), method='perturbation')
+    chi_010 = crysfipy.susceptibility(cefion, temperatures , (0,1,0), method='perturbation')
+    chi_001 = crysfipy.susceptibility(cefion, temperatures , (0,0,1), method='perturbation')
 
 #    print(chi_100)
 
@@ -76,7 +92,7 @@ if calculateMagnetism:
     ax.legend(title=f'ErB$_2$ perturbation')
 
     # Set up the magnetization
-    ax = axs[2]
+    ax = axs[3]
 
     ax.set_xlabel('Applied field (T)')
     ax.set_ylabel('Magnetic moment ($\mu_B$/ion)')
@@ -84,9 +100,9 @@ if calculateMagnetism:
     fields = np.linspace(1e-5, 1000, 101)
     Ma, Mb, Mc = [], [], []
     for field in fields:
-        Ma.append(ms.crysfipy.magnetization(cefion, temperature, (field,0,0))[0])
-        Mb.append(ms.crysfipy.magnetization(cefion, temperature, (0,field,0))[1])
-        Mc.append(ms.crysfipy.magnetization(cefion, temperature, (0,0,field))[2])
+        Ma.append(crysfipy.magnetization(cefion, temperature, (field,0,0))[0])
+        Mb.append(crysfipy.magnetization(cefion, temperature, (0,field,0))[1])
+        Mc.append(crysfipy.magnetization(cefion, temperature, (0,0,field))[2])
 
 
 
@@ -96,4 +112,4 @@ if calculateMagnetism:
 
     ax.legend(title=f'ErB$_2$, T={temperature} K')
 
-    fig.savefig(r'C:\Users\Stekiel\Documents\GitHub\mikibox\examples\ErB2-magnetism-mikibox.png',dpi=200)
+    fig.savefig(r'C:\Users\Stekiel\Documents\GitHub\crysfipy\examples\ErB2-magnetism-mikibox.png',dpi=200)
